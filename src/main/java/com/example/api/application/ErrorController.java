a4725a7a57e3fbed497f631dfd1fe1aa5ec0df10
Agop Hurmuz <agophurmuz@gmail.com>
2022-04-04 23:23:32 -0300

    errorController fix

--- src/main/java/com/example/api/application/ErrorController.java
+++ src/main/java/com/example/api/application/ErrorController.java
@@ -2,9 +2,11 @@ package com.example.api.application;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+@RestController
 public class ErrorController {
 
     @RequestMapping("/error")
